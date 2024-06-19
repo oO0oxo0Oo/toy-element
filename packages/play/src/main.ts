5122061049e242fc453087b0c15a6a65c7f9ca53
xj <2311877257@qq.com>
2024-06-19 18:52:07 +0800

    chore:version update

--- packages/play/src/main.ts
+++ packages/play/src/main.ts
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 // 导入 'toy-element' 库，其中包含一组预先封装好的 Vue 组件
 import ToyElement from 'toy-element'
-import "o0o-toy-element-o0o/dist/index.css"
+import "toy-element/dist/index.css"
 
 createApp(App).use(ToyElement).mount('#app')
